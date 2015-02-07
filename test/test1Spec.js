ed48907b1d3edc39d306b940cd055ce5d119d7fd
adamjakab <jakabadambalazs@gmail.com>
2015-02-07 18:47:55 +0100

    init

---
+++ test/test1Spec.js
@@ -0,0 +1,11 @@
+define([
+
+], function () {
+    describe("Tests1 Tests", function() {
+
+        it("should work", function() {
+            expect(true).toBeTruthy();
+        });
+
+    });
+});
